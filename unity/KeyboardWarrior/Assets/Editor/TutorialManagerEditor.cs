a1378151380b2149fb98856da5ff817d28bc9a5b
jonmura <jonmura1999@gmail.com>
2020-02-05 10:50:22 -0500

    Started on tutorial scripts & custom editor for tutorial

---
+++ unity/KeyboardWarrior/Assets/Editor/TutorialManagerEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomEditor(typeof(TutorialManager))]
+public class TutorialManagerEditor : Editor
+{
+    private ReorderableList dialogList;
+    private ReorderableList advanceList;
+
+    private void OnEnable()
+    {
+        dialogList = new ReorderableList(serializedObject, serializedObject.FindProperty("dialog"), true, true, true, true);
+        dialogList.drawHeaderCallback = (Rect rect) =>
+        {
+            EditorGUI.LabelField(rect, "Tutorial Dialog");
+        };
+        dialogList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            SerializedProperty element = dialogList.serializedProperty.GetArrayElementAtIndex(index);
+
+            EditorGUI.PropertyField(rect, element, new GUIContent());
+        };
+
+
+        advanceList = new ReorderableList(serializedObject, serializedObject.FindProperty("advanceWords"), true, true, true, true);
+        advanceList.drawHeaderCallback = (Rect rect) =>
+        {
+            EditorGUI.LabelField(rect, "Advance Keywords");
+        };
+        advanceList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            SerializedProperty element = advanceList.serializedProperty.GetArrayElementAtIndex(index);
+
+            EditorGUI.PropertyField(rect, element, new GUIContent());
+        };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        dialogList.DoLayoutList();
+        advanceList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ unity/KeyboardWarrior/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField][HideInInspector]
+    private List<string> dialog;
+    [SerializeField][HideInInspector]
+    private List<string> advanceWords;
+
+    public Text dialogBox;
+    public GameObject advanceBox;
+    private Text advanceBoxText;
+    private Text advanceBoxOverlay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        advanceBoxText = advanceBox.transform.FindChild("Text").GetComponent<Text>();
+        //float width = advanceBoxText.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
